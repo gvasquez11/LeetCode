258db1cd522f2ef2289ef9b77c7684f2e1671c01
gvasquez11 <vasquez.greg16@gmail.com>
2023-06-07 16:04:49 -0700

    771. Jewels and Stones

---
+++ C++/771. Jewels and Stones.cpp
@@ -0,0 +1,58 @@
+/*
+771. Jewels and Stones
+
+You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+
+Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+
+
+Example 1:
+
+Input: jewels = "aA", stones = "aAAbbbb"
+Output: 3
+Example 2:
+
+Input: jewels = "z", stones = "ZZ"
+Output: 0
+
+
+Constraints:
+
+1 <= jewels.length, stones.length <= 50
+jewels and stones consist of only English letters.
+All the characters of jewels are unique.
+
+
+*/
+
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int numJewelsInStones(string jewels, string stones)
+    {
+
+        unordered_map<char, int> jewelMap;
+        int counter = 0;
+
+        for (auto jewel : jewels)
+        {
+            jewelMap[jewel] = 0;
+        }
+
+        for (auto stone : stones)
+        {
+            if (jewelMap.count(stone))
+            {
+                counter++;
+                jewelMap[stone]++;
+            }
+        }
+        return counter;
+    }
+};

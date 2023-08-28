9471e15f4bb910d1b597d14471c0f5b28007e960
gvasquez11 <vasquez.greg16@gmail.com>
2023-08-28 12:27:38 -0700

    500. Keyboard Row

--- C++/500. Keyboard Row.cpp
+++ C++/500. Keyboard Row.cpp
@@ -32,3 +32,51 @@ Constraints:
 words[i] consists of English letters (both lowercase and uppercase).
 
 */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+    vector<string> findWords(vector<string> &words)
+    {
+
+        string row1 = "qwertyuiop";
+        string row2 = "asdfghjkl";
+        string row3 = "zxcvbnm";
+
+        vector<string> result;
+
+        for (auto word : words)
+        {
+
+            int find1 = 0;
+            int find2 = 0;
+            int find3 = 0;
+
+            for (int i = 0; i < word.size(); i++)
+            {
+                char letter = (char)tolower(word[i]);
+
+                //.find uses size_t which is an unsigned integer type so it can never be negative
+                // if we use int for .find it can still cause problems becuase it
+                // can return a value greater than INT_MAX but not npos
+                // So the best option is to compare to npos(no position) instead like the following
+                if (row1.find(letter) != string::npos)
+                    find1++;
+                else if (row2.find(letter) != string::npos)
+                    find2++;
+                if (row3.find(letter) != string::npos)
+                    find3++;
+            }
+
+            if (find1 == word.size() || find2 == word.size() || find3 == word.size())
+                result.push_back(word);
+        }
+
+        return result;
+    }
+};

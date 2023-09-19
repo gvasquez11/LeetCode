d2dd6b64552eade4e2294cc75b38ad606697a85e
gvasquez11 <vasquez.greg16@gmail.com>
2023-09-19 10:36:31 -0700

    26. Remove Duplicates from Sorted Array

--- C++/26. Remove Duplicates from Sorted Array.cpp
+++ C++/26. Remove Duplicates from Sorted Array.cpp
@@ -45,3 +45,29 @@ Constraints:
 nums is sorted in non-decreasing order.
 
 */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int removeDuplicates(vector<int> &nums)
+    {
+
+        int pos = 0;
+
+        for (int i = 1; i < nums.size(); i++)
+        {
+            if (nums[i] != nums[pos])
+            {
+                pos++;
+                nums[pos] = nums[i];
+            }
+        }
+
+        return pos + 1;
+    }
+};

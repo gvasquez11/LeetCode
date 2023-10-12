e1493acbdaa26051d40d2799231aaf37ac63e268
gvasquez11 <vasquez.greg16@gmail.com>
2023-10-11 19:59:23 -0700

    125. Valid Palindrome

--- C++/125. Valid Palindrome.cpp
+++ C++/125. Valid Palindrome.cpp
@@ -61,3 +61,37 @@ public:
         return temp == wordReverse;
     }
 };
+
+// Two pointer solution
+
+class Solution
+{
+public:
+    bool isPalindrome(string s)
+    {
+
+        int left = 0;
+        int right = s.size() - 1;
+
+        while (left < right)
+        {
+            while (left < right && !isalnum(s[left]))
+            {
+                left++;
+            }
+            while (left < right && !isalnum(s[right]))
+            {
+                right--;
+            }
+            if (tolower(s[left]) != tolower(s[right]))
+            {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+};
